0a93ddafab3f12be36fbcb8a62d7eaeba64a618f
shaikroufa <shaikroufa16@gmail.com>
2026-07-19 17:13:27 +0530

    Add files via upload

---
+++ Shopping.java
@@ -0,0 +1,27 @@
+import java .util.Scanner;
+public class Shopping {
+public static void main(String[] args){
+    Scanner sc =new Scanner(System.in);
+    String item;
+    int quantity;
+    char currency='$';
+      Double price=25.1;
+    Double total;
+
+    System.out.print("What wouold you like to buy: ");
+    item = sc.nextLine();
+    System.out.print("How many items would you buy: ");
+    quantity = sc.nextInt();
+    System.out.println("price of one  item is : " + price +currency);
+    
+    total=price*quantity;
+
+    System.out.println("The total bill of your's is: "+total +currency);
+
+
+    sc.close();
+
+
+
+    }
+}
